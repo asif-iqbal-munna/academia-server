4ba7494f5d52d73ec373f03e1878139996ffc198
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-10-28 02:26:25 +0600

    data inserted to the mongo server

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
-/* // MongoDB URI With Username and Password that are stored in environment variable
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.bauja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB URI With Username and Password that are stored in environment variable
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bauja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 //    Set Up Client With That URI/ User
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); */
-
-app.get("/", (req, res) => {
-  console.log("server running");
-  res.send("from the server");
 });
+
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("OnlinePrograms");
+    const programCollection = database.collection("Programs");
+
+    // const result = await courseCollection.insertOne("hi there");
+    app.post("/programs", async (req, res) => {
+      const program = req.body;
+      const result = await programCollection.insertOne(program);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
+
+//  Tested if working in local server
+// app.get("/", (req, res) => {
+//   console.log("server running");
+//   res.send("from the server");
+// });
 
 app.listen(port, () => {
   console.log("Listening to the port", port);
